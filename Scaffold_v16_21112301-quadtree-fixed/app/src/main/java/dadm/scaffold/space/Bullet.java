c6a8519bfc2cafbaa6b8b6590f4944f4fa2b73e0
Jorge <jgaliano2000@gmail.com>
2021-11-26 18:50:48 +0100

    [BULLETS] Nuevos diseños de bala

--- Scaffold_v16_21112301-quadtree-fixed/app/src/main/java/dadm/scaffold/space/Bullet.java
+++ Scaffold_v16_21112301-quadtree-fixed/app/src/main/java/dadm/scaffold/space/Bullet.java
@@ -13,7 +13,7 @@ public class Bullet extends Sprite {
     private SpaceShipPlayer parent;
 
     public Bullet(GameEngine gameEngine){
-        super(gameEngine, R.drawable.bullet,30,8,0,0);
+        super(gameEngine, R.drawable.maletita,0,0,0,0);
 
         speedFactor = gameEngine.pixelFactor * -300d / 1000d;
     }

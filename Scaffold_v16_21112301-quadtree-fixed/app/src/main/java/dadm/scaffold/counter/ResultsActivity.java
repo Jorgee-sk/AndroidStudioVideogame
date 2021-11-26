97ffa7b296b02d8364007d55fc6fe7627b073cbc
kalomano <sowerpablo@gmail.com>
2021-11-26 18:25:01 +0100

    Estetica MainMenu

--- Scaffold_v16_21112301-quadtree-fixed/app/src/main/java/dadm/scaffold/counter/ResultsActivity.java
+++ Scaffold_v16_21112301-quadtree-fixed/app/src/main/java/dadm/scaffold/counter/ResultsActivity.java
@@ -1,9 +1,14 @@
 package dadm.scaffold.counter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import dadm.scaffold.R;
+import dadm.scaffold.ScaffoldActivity;
 
 public class ResultsActivity extends AppCompatActivity {
 
@@ -11,5 +16,32 @@ public class ResultsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        int puntuacion = getIntent().getExtras().getInt("puntuacion");
+
+
+        TextView intPuntuacion = (TextView) findViewById(R.id.intPuntuacion);
+        intPuntuacion.setText(puntuacion + "");
+
+
+
+        ImageButton btnMainMenu = (ImageButton) findViewById(R.id.ButtonMainMenu);
+        btnMainMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), ScaffoldActivity.class);
+                startActivity(intent);
+
+
+            }
+
+
+        });
+
+
+
+
+
     }
 }
